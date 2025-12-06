57532411be00b5599da7c9fac7fd607906011df5
e.rahbari <e.rahbari@payampardaz.local>
2025-12-06 17:43:52 +0330

    back: add simple user service

--- backend/hsm-server/src/users/users.service.ts
+++ backend/hsm-server/src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+//TODO: must be declared and moved to seperate folders
+export type User = {
+  userId: number;
+  userName: string;
+  password: string;
+};
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  //TODO: whole class must be use the db setup for user crud
+  private readonly Users: User[] = [
+    { userId: 1, userName: 'john', password: 'changeme' },
+  ];
+
+  findOne(userName: string): User | undefined {
+    return this.Users.find((user) => user.userName === userName);
+  }
+}
