82ea38ed5ee8bec9c86174366b09eabd5d0bfe0f
e.rahbari <e.rahbari@payampardaz.local>
2025-12-06 17:57:53 +0330

    back: add auth login path

--- backend/hsm-server/src/auth/auth.controller.ts
+++ backend/hsm-server/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  //TODO: declaring dtos for user correctly
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.userName, signInDto.password);
+  }
+}
